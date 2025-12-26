20c0b22938e119d311ad2d9b246a6a4370bc689a
lll-DEADLY-VENOM-lll <shivampatel425685@gmail.com>
2025-12-26 13:52:37 +0530

    NEO ⚡

--- Spy/core/userbot.py
+++ Spy/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Dil_Feelings_Will1")
-                await self.one.join_chat("Dil_Feelings_Will1")
+                await self.one.join_chat("NOBITA_SUPPORT")
+                await self.one.join_chat("NOBITA_SUPPORT")
             except:
                 pass
             assistants.append(1)
