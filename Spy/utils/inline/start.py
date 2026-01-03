51936bb95197ca8f021f757ef39836f3dd811941
lll-DEADLY-VENOM-lll <shivampatel425685@gmail.com>
2026-01-03 10:01:34 +0530

    0.2 version

--- Spy/utils/inline/start.py
+++ Spy/utils/inline/start.py
@@ -1,16 +1,22 @@
 from pyrogram.types import InlineKeyboardButton
-
 import config
-from Spy import app
+from Spy import app  # Using your old import structure
 
 
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
+                url=f"https://t.me/{app.username}?startgroup=true",
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+        [
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"),
+        ],
+        [
+            InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
@@ -20,17 +26,35 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="dil_spy"),
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
+            InlineKeyboardButton(text="𝐆𝚁𝙾𝚄𝙿✨", url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="𝐌ᴏʀᴇ🥀", url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton("• ʙᴏᴛ ɪɴғᴏ •", callback_data="bot_info_data"),
+            InlineKeyboardButton(
+                text="۞ 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="• ʙᴏᴛ ɪɴғᴏ •", callback_data="bot_info_data"
+            ),
+        ],
+    ]
+    return buttons
+
+
+def alive_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="✿︎ ᴀᴅᴅ ᴍᴇ ✿︎", url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text="𝐒ᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
