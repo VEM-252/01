4ecfc1f1934d4c3fef9222014eba54801b76f0ba
lll-DEADLY-VENOM-lll <shivampatel425685@gmail.com>
2026-01-03 13:57:15 +0530

    0.2 version

--- Spy/utils/inline/play.py
+++ Spy/utils/inline/play.py
@@ -74,8 +74,8 @@ def stream_markupp(_, videoid, chat_id, *args, **kwargs):
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay, *args, **kwargs):
     buttons = [
         [
-            InlineKeyboardButton(text=_["P_B_1"], callback_data=f"VIPPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}"),
-            InlineKeyboardButton(text=_["P_B_2"], callback_data=f"VIPPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}"),
+            InlineKeyboardButton(text=_["P_B_1"], callback_data=f"SagarPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}"),
+            InlineKeyboardButton(text=_["P_B_2"], callback_data=f"SagarPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"forceclose {videoid}|{user_id}")],
     ]
